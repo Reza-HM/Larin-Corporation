5a5e056d64233f57132d8ba654eb7dd16da1f031
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-05 18:55:35 +0330

    coding customers section (cube effect slider)

--- src/Components/Templates/Index/Services.tsx
+++ src/Components/Templates/Index/Services.tsx
@@ -2,7 +2,7 @@ import Service from "../../Modules/Service";
 
 const Services = () => {
   return (
-    <div className="m-20 rounded-3xl bg-gradient-to-t from-orange-100">
+    <div className="mt-20 rounded-3xl bg-gradient-to-t from-orange-100">
       <div className="container p-20 max-w-screen-2xl">
         <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-4 gap-8">
           <Service />
--- src/Pages/Index.tsx
+++ src/Pages/Index.tsx
@@ -3,6 +3,7 @@ import Header from "../Components/Templates/Index/Header";
 import SectionHeader from "../Components/Modules/SectionHeader";
 import Services from "../Components/Templates/Index/Services";
 import Projects from "../Components/Templates/Index/Projects";
+import Customers from "../Components/Templates/Index/Customers";
 
 const Index: FC = () => {
   return (
@@ -12,6 +13,8 @@ const Index: FC = () => {
       <Services />
       <SectionHeader englishTitle="Projects" persianTitle="پروژه‌های لارین" />
       <Projects />
+      <SectionHeader englishTitle="Customers" persianTitle="نظرات مشتریان" />
+      <Customers />
     </div>
   );
 };
