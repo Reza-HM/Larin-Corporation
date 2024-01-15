baeeb5b50cc82741ed17a6ea0f6b6c5423367816
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-16 00:16:51 +0330

    edit index.js file

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import jsonServer from "json-server";
 
 const server = express();
-const router = jsonServer.router("/src/data/db.json");
+const router = jsonServer.router("./src/data/db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
